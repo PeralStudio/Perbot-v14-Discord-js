90f8c21d1b1a227c2bcae9ed5b04cd7a89d5a444
Alberto <info@peralstudio.com>
2022-08-16 16:41:51 +0200

    Update youtube.js

--- functions/youtube.js
+++ functions/youtube.js
@@ -24,28 +24,28 @@ const setIntervalYoutube = async (client, userId) => {
             titulo: videos.items[0].title,
         });
 
-        const embed = new EmbedBuilder()
-            .setAuthor({
-                name: `${videos.items[0].author}`,
-                iconURL: channel.authorThumbnails[0].url,
-            })
-            .setTitle(`${videos.items[0].title}`)
-            .setThumbnail(`${channel.authorThumbnails[0].url}`)
-            .setURL(
-                `https://www.youtube.com/watch?v=${videos.items[0].videoId}`
-            )
-            .setImage(
-                `${
-                    videos.items[0].videoThumbnails[2].url ||
-                    videos.items[0].videoThumbnails[0].url
-                }`
-            )
-            .setTimestamp()
-            .setFooter({
-                text: versionbot,
-                iconURL: client.user.displayAvatarURL(),
-            })
-            .setColor(0x00ff00);
+        // const embed = new EmbedBuilder()
+        //     .setAuthor({
+        //         name: `${videos.items[0].author}`,
+        //         iconURL: channel.authorThumbnails[0].url,
+        //     })
+        //     .setTitle(`${videos.items[0].title}`)
+        //     .setThumbnail(`${channel.authorThumbnails[0].url}`)
+        //     .setURL(
+        //         `https://www.youtube.com/watch?v=${videos.items[0].videoId}`
+        //     )
+        //     .setImage(
+        //         `${
+        //             videos.items[0].videoThumbnails[2].url ||
+        //             videos.items[0].videoThumbnails[0].url
+        //         }`
+        //     )
+        //     .setTimestamp()
+        //     .setFooter({
+        //         text: versionbot,
+        //         iconURL: client.user.displayAvatarURL(),
+        //     })
+        //     .setColor(0x00ff00);
 
         if (!data) {
             const newData = new youtube({
@@ -55,13 +55,13 @@ const setIntervalYoutube = async (client, userId) => {
 
             if (ultimoVideo.liveNow === true) {
                 await client.channels.cache.get("1009104544686407730").send({
-                    content: `<@209338137346834433> - **${videos.items[0].author}** esta en **directo**  \n  `,
-                    embeds: [embed],
+                    content: `<@209338137346834433> - **${videos.items[0].author}** esta en **directo** \n https://www.youtube.com/watch?v=${videos.items[0].videoId} `,
+                    // embeds: [embed],
                 });
             } else {
                 await client.channels.cache.get("1009104544686407730").send({
-                    content: `<@209338137346834433> - **${videos.items[0].author}** ha subido un **nuevo video**  \n  `,
-                    embeds: [embed],
+                    content: `<@209338137346834433> - **${videos.items[0].author}** ha subido un **nuevo video** \n https://www.youtube.com/watch?v=${videos.items[0].videoId} `,
+                    // embeds: [embed],
                 });
             }
 
