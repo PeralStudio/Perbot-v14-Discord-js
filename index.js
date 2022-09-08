e025815907987cc7ca9de837c349ef611872c56d
Alberto <info@peralstudio.com>
2022-09-08 17:35:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1181,7 +1181,7 @@ client.on("interactionCreate", async (interaction) => {
                                     `https://www.leagueoflegends.com/es-es/news/game-updates/patch-${patchVersionWithDash}-notes/`
                                 )
                                 .setThumbnail(
-                                    "https://peralstudio.com/images/lol-logo.png"
+                                    "https://peralstudio.com/images/lol-logo.jpg"
                                 )
                                 .setImage(imgPathForEmbed)
                                 .setTimestamp()
