b9f6091aca9c2872436b23bf9103eacbd34342de
Alberto <info@peralstudio.com>
2022-09-10 00:19:15 +0200

    Update elrellano.js

--- commands/elrellano.js
+++ commands/elrellano.js
@@ -2,11 +2,11 @@ import { SlashCommandBuilder } from "discord.js";
 
 const elrellanoCommand = new SlashCommandBuilder()
     .setName("elrellano")
-    .setDescription("Video de Elrellano del 1 al 6")
+    .setDescription("Vídeo de Elrellano del 1 al 6")
     .addNumberOption((option) =>
         option
             .setName("1-6")
-            .setDescription("Video del 1 al 6 de elrellano.com")
+            .setDescription("Vídeo del 1 al 6 de elrellano.com")
             .setRequired(true)
     );
 
