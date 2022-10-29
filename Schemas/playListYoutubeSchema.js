5c50441de38d901daf612963a244bc8f5e6b34c3
Alberto <info@peralstudio.com>
2022-10-29 22:38:33 +0200

    Update PlayListYT Schema MongoDB

--- Schemas/playListYoutubeSchema.js
+++ Schemas/playListYoutubeSchema.js
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
 
 const playListYoutubeSchema = new mongoose.Schema({
+    playListAuthor: {
+        type: String,
+    },
     playListAuthorID: {
+        type: String,
+    },
+    playListTitle: {
+        type: String,
+    },
+    playlistUrl: {
         type: String,
     },
     playListCount: {
         type: Number,
-    },
-    playlistUrl: {
-        type: String,
     },
 });
 
